e0f828bfc4e5ba32c5e2ea4cf0a16163174755bf
Andre Gonçalves <dregoncalves22@gmail.com>
2024-04-01 11:50:10 -0300

    Initial commit

---
+++ conexao.php
@@ -0,0 +1,5 @@
+<?php 
+//conexao com o banco de dados usando PDO
+$con = new PDO('mysql:host=localhost;dbname=galize', 'root', '');
+
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include_once('conexao.php');
+
+include_once('layout/header.php');
+
+?>
+
+<div class="card mt-4">
+    <div class="card-header">
+        <h1 class="card-title">Teste</h1>
+    </div>
+    <div class="card-body">
+        <p class="card-text">Tentando fazer um CRUD com PHP e Bootstrap</p>
+    </div>
+</div>
+
+<?php include_once('layout/footer.php'); ?>
